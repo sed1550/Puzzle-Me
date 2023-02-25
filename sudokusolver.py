fe712fd096785ba88b061fc042a17c07589ac288
Sarah D souza <sed1550@rit.edu>
2023-02-25 15:46:23 -0500

    Record possible values for all empty cells in a dictionary

--- sudokusolver.py
+++ sudokusolver.py
@@ -2,6 +2,58 @@ ROWS = 9
 COLS = 9
 
 
+def is_valid_value(value, board, cell_row, cell_col):
+    # cell_row, cell_col = position
+    # check rows
+    for row in range(ROWS):
+        if board[row][cell_col] == value:
+            return False
+
+    # check columns
+    for col in range(COLS):
+        if board[cell_row][col] == value:
+            return False
+
+    # check enclosing grid
+    grid_starting_row = 3 * (cell_row // 3)
+    grid_starting_col = 3 * (cell_col // 3)
+    grid_ending_row = grid_starting_row + 3
+    grid_ending_col = grid_starting_col + 3
+    for row in range(grid_starting_row, grid_ending_row):
+        for col in range(grid_starting_col, grid_ending_col):
+            if board[row][col] == value:
+                return False
+
+    return True
+
+
+def get_possible_values_list(board, row, col):
+    possible_values = []
+    for value in range(1, 10):
+        if is_valid_value(value, board, row, col):
+            possible_values.append(value)
+    return possible_values
+
+
+def record_possible_values_for_empty_cells(board):
+    possible_values_dict = {}
+    for row in range(ROWS):
+        for col in range(COLS):
+            if board[row][col] == 0:
+                possible_values_list = get_possible_values_list(board, row, col)
+                possible_values_dict[(row, col)] = possible_values_list
+
+
+def find_least_possible_values_cell():
+    for row in range(ROWS):
+        for col in range(COLS):
+            pass
+
+
+def solve():
+    pass
+
+
 def print_board(board):
     for row in range(ROWS):
         if row != 0 and row % 3 == 0:
