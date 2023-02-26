0acb5b3622a3ccfae1dae60dd411aa190087b451
Sarah D souza <sed1550@rit.edu>
2023-02-26 18:51:46 -0500

    Solve by filling cells with least number of possible values first

--- sudokusolver.py
+++ sudokusolver.py
@@ -36,22 +36,48 @@ def get_possible_values_list(board, row, col):
 
 
 def record_possible_values_for_empty_cells(board):
-    possible_values_dict = {}
+    possible_values_tracker = {}
     for row in range(ROWS):
         for col in range(COLS):
             if board[row][col] == 0:
                 possible_values_list = get_possible_values_list(board, row, col)
-                possible_values_dict[(row, col)] = possible_values_list
+                possible_values_tracker[(row, col)] = possible_values_list
+    return possible_values_tracker
 
 
-def find_least_possible_values_cell():
+def find_least_possible_values_cell(board, possible_values_tracker):
+    # initialize default values
+    least_number_of_possible_values = 9
+    cell_position = None
     for row in range(ROWS):
         for col in range(COLS):
-            pass
+            if board[row][col] == 0:
+                current_cell_possible_values_list = possible_values_tracker[(row, col)]
+                current_possible_values_num = len(current_cell_possible_values_list)
+                if current_possible_values_num < least_number_of_possible_values:
+                    least_number_of_possible_values = current_possible_values_num
+                    cell_position = row, col
+    return cell_position
 
 
-def solve():
-    pass
+def solve_with_mrv(board):
+    print("New board:")
+    print_board(board)
+    possible_values_tracker = record_possible_values_for_empty_cells(board)
+    print(possible_values_tracker)
+    empty_cell = find_least_possible_values_cell(board, possible_values_tracker)
+    if not empty_cell:
+        return True
+    print(empty_cell)
+    empty_cell_pv_list = possible_values_tracker.get(empty_cell)
+    empty_cell_row, empty_cell_col = empty_cell
+    for value in empty_cell_pv_list:
+        board[empty_cell_row][empty_cell_col] = value
+        # check is_valid_value?
+        if solve_with_mrv(board):
+            return True
+        board[empty_cell_row][empty_cell_col] = 0
+    return False
 
 
 def print_board(board):
@@ -78,8 +104,8 @@ def main():
                    [9, 2, 5, 0, 0, 0, 8, 7, 4]]
 
     print_board(input_board)
+    solve_with_mrv(input_board)
     print("Solved:")
-    # solve(input_board)
     print_board(input_board)
 
 
