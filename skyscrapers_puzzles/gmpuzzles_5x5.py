83a1f5fb9cdd21286c0156feecdc130c92745f08
Sarah D souza <sed1550@rit.edu>
2023-05-10 13:35:16 -0400

    Added puzzles for testing

---
+++ skyscrapers_puzzles/gmpuzzles_5x5.py
@@ -0,0 +1,61 @@
+# Borders in the order of N, E, S, W
+
+# https://www.gmpuzzles.com/blog/2020/09/skyscrapers-by-ashish-kumar-2/
+# 2 stars
+gm_borders_5x5_1 = [[0, 2, 0, 2, 0],
+                    [2, 0, 2, 0, 0],
+                    [2, 0, 0, 2, 0],
+                    [0, 2, 2, 0, 2]]
+gm_board_5x5_1 = [[0, 0, 0, 0, 0],
+                  [0, 0, 0, 0, 0],
+                  [0, 0, 0, 0, 0],
+                  [0, 0, 0, 0, 0],
+                  [0, 0, 0, 0, 0]]
+
+# https://www.gmpuzzles.com/blog/2020/08/skyscrapers-by-murat-can-tonta-3/
+# 1 star
+gm_borders_5x5_2 = [[0, 0, 2, 2, 0],
+                    [0, 0, 0, 0, 0],
+                    [3, 0, 0, 0, 0],
+                    [4, 4, 0, 0, 3]]
+gm_board_5x5_2 = [[0, 0, 0, 0, 0],
+                  [0, 0, 0, 0, 0],
+                  [0, 0, 0, 0, 0],
+                  [0, 0, 0, 0, 0],
+                  [0, 0, 0, 0, 0]]
+
+# https://www.gmpuzzles.com/blog/2021/02/skyscrapers-haido-by-jinhoo-ahn/
+# 2 stars
+gm_borders_5x5_3 = [[0, 0, 2, 2, 2],
+                    [0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0],
+                    [0, 0, 3, 3, 3]]
+gm_board_5x5_3 = [[0, 0, 0, 0, 0],
+                  [0, 0, 0, 0, 0],
+                  [0, 0, 0, 0, 0],
+                  [0, 0, 0, 0, 0],
+                  [0, 0, 0, 0, 0]]
+
+# https://www.gmpuzzles.com/blog/2022/05/skyscrapers-by-salih-alan-4/
+# 1.5 stars
+gm_borders_5x5_4 = [[0, 2, 5, 0, 0],
+                    [0, 0, 2, 4, 0],
+                    [0, 0, 0, 2, 2],
+                    [0, 2, 3, 0, 0]]
+gm_board_5x5_4 = [[0, 0, 0, 0, 0],
+                  [0, 0, 0, 0, 0],
+                  [0, 0, 0, 0, 0],
+                  [0, 0, 0, 0, 0],
+                  [0, 0, 0, 0, 0]]
+
+# https://www.gmpuzzles.com/blog/2019/05/skyscrapers-by-murat-can-tonta/
+# 1/1.5 stars
+gm_borders_5x5_5 = [[4, 0, 0, 4, 0],
+                    [0, 4, 0, 0, 0],
+                    [0, 0, 0, 0, 0],
+                    [0, 0, 4, 0, 0]]
+gm_board_5x5_5 = [[0, 0, 0, 0, 0],
+                  [0, 0, 0, 0, 0],
+                  [0, 0, 0, 0, 0],
+                  [0, 0, 0, 0, 0],
+                  [0, 0, 0, 0, 0]]
